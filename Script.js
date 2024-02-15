d6cf8c57f101b06d18d6f8e870ef60c6c750249a
MaspenaMD <105624436+MaspenaMD@users.noreply.github.com>
2024-02-15 15:27:13 +0700

    Myhappyness

---
+++ Script.js
@@ -0,0 +1,83 @@
+function showmn() {
+  const tabbar = document.querySelector(".menu-bar").style;
+  tabbar.left=0;
+  tabbar.transition="left 0.3s ease-in-out"
+}
+
+function closemn() {
+  const tabbar = document.querySelector(".menu-bar").style;
+  tabbar.left="-160px";
+  tabbar.transition="left 0.3s ease-in-out"
+}
+
+function klop1() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk1");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop2() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk2");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop3() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk3");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop4() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk4");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop5() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk5");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop6() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk6");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop7() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk7");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop8() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk8");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop9() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk9");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+function klop10() {
+  const jdl = document.querySelector("h2"),
+        prg = document.getElementById("prgrf"),
+        Bab = document.getElementById("lk10");
+  jdl.innerHTML = Bab.innerHTML;
+  prg.innerHTML = Bab.innerHTML;
+}
+
